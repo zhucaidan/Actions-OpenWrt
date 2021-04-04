b5c52e390d97d4f9e08fb231e4483df4ca534582
zhucaidan <47970938+zhucaidan@users.noreply.github.com>
2021-04-03 23:34:31 -0700

    2021-4-3

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# Modify default IP# Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Remove v2ray-plugin
+rm -rf openwrt/package/lean/v2ray-plugin
